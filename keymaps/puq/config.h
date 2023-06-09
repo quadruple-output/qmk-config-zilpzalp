efda504b7e634be6492802b936b35d105dc08f0b
quadruple-output <57874618+quadruple-output@users.noreply.github.com>
2023-06-09 21:44:14 +0200

    increase COMBO_TERM for vertical combos

--- keymaps/puq/config.h
+++ keymaps/puq/config.h
@@ -1,5 +1,10 @@
 #pragma once
 
-#define COMBO_TERM 50 // default: 50
+// Most combos are intentionally defined for two adjacent keys of the same column, such that rolling
+// key effects do not accidentally trigger a combo. This allows us to choose a rather long combo
+// term, which can help if the finger does not precisely hit the gap between the keys.
+#define COMBO_TERM 100 // default: 50
+
 #define TAPPING_TERM 200 // default: 200
+
 #define PERMISSIVE_HOLD
