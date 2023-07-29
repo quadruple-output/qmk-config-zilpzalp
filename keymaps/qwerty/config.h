1811a3f8dda5ed842f6579cb4d2d593ac3fd2d78
quadruple-output <57874618+quadruple-output@users.noreply.github.com>
2023-07-29 21:35:36 +0200

    add QWERTY keyboard layout

---
+++ keymaps/qwerty/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define COMBO_TERM 40 // default: 50
+// #define COMBO_MUST_TAP_PER_COMBO
+
+#define TAPPING_TERM 200 // default: 200
+// #define TAPPING_TOGGLE 2 // require only two taps (default=5) to toggle layer
+
+// #define PERMISSIVE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS
+#define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
